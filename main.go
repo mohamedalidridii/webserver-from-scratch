71d5c9e4c2001787bcd525dcbb374e0c657d87f2
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2025-09-22 20:05:59 +0100

    adding a function getlineschannel

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import "os"
 import "fmt"
-//import "io"
+import "io"
 import "log"
 import "bytes"
 
-func main () {
-	f, err := os.Open("main.txt")
-	if err != nil {
-	log.Fatal("error", "error", err)
-	}
+
+func getLinesChannel(f io.ReadCloser)<-chan string {
+	out := make(chan string, 1)
+	
+	go func(){
+		defer f.Close()
+		defer close(out)
+
+	
 	str := ""
 	for {
 		data := make([]byte, 8)
@@ -22,13 +26,27 @@ func main () {
 		if i := bytes.IndexByte(data, '\n'); i != -1 {
 			str += string(data[:i])
 			data = data[i + 1:]
-			fmt.Printf("read:  %s\n", str)
+			out <- str
 			str=""
 		}
 		str += string(data) 
 	}
 	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str) 
+		out <- str
+	}
+	}()
+
+	return out
+} 
+
+func main () {
+	f, err := os.Open("main.txt")
+	if err != nil {
+	log.Fatal("error", "error", err)
+	}
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
 
